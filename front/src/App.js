75d3980913dc1c5bd7f9255bf96d22652ae10093
Jesus <104527491+FuryBaco@users.noreply.github.com>
2022-10-18 01:32:26 -0500

    Clase 7
    Actualización clase 7, inicio de creación del front, instalación react y componentes de react, visualización en Chrome, creación del logo Super Master's

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Cacharreria Super Master's</h1>
+    <img src="./images/super-masters-logo.png" alt="Aca deberia ir un logo" class="App-logo"></img>
+    </div>
+  );
+}
+
+export default App;
