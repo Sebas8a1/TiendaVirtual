56ba72fc889990352185fadb82b580dd9a3f4b3a
Sebas8a1 <sebastian8a@hotmail.com>
2022-10-29 02:13:49 -0500

    Actualizacion mockups

--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -1,33 +1,46 @@
 import React, { Fragment } from 'react'
+import "../../App.css"
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-    <Fragment>
-        <nav className="navbar row navbar-expand-lg navbar-dark bg-secondary">
-            <div className="col-12 col-md-3">
-                <div className="navbar-brand">
-                    <img src=".\images\bookstore1.png" alt="Aca deberia ir un logo" class="App-logo"></img>
-            </div>
-        </div>
-
-            <div className="col-12 col-md-6 mt-2 mt-md-0">
-                <div className="input-group">
-                    <input type="text" id="search_field" class="form-control" placeholder="Buscar productos..."/>
-                    <div class="input-group-append">
-                        <button id="search_btn" className="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+        <Fragment>
+            <nav className="navbar row navbar-expand-lg navbar-dark bg-secondary">
+                <div className="col-12 col-md-3">
+                    <div className="navbar-brand">
+                        <Link to="/"><img src=".\images\bookstore1.png" alt="Aca deberia ir un logo" class="App-logo"></img></Link>
                     </div>
                 </div>
-            </div>
 
-            <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                <button className="button" id="login_button">Iniciar Sesión</button>
-                <i class="fa-solid fa-cart-shopping"></i>
-                <span className="ml-1" id="cart_count">2</span>
-            </div>
-            
-        </nav>
-    </Fragment>
-    
+                <div className="col-12 col-md-6 mt-2 mt-md-0">
+                    <div className="input-group">
+                        <input type="text" id="search_field" class="form-control" placeholder="Buscar productos..." />
+                        <div class="input-group-append">
+                            <button id="search_btn" className="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+                    <div className="ml-4 dropdown d-inline">
+                        <Link to="#!" className="btn dropdown-toggle mr-4 mr-4" type="button"
+                            id="dropDownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span>Panel de control</span> </Link>
+                        <div className="dropdown-menu" aria-labelledby="dropDownMenu">
+                            <Link className="dropdown-item" to="/admin/dashboard">Adm. de Productos</Link>
+                            <Link className="dropdown-item" to="#!">Pedidos</Link>
+                            <Link className="dropdown-item" to="#!">Mi Cuenta</Link>
+                            <Link className="dropdown-item" to="#!">Cerrar Sesión</Link>
+                        </div>
+                    </div>
+
+                    <Link to="/cart"><i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="false"></i>
+                        <span className="ml-1" id="cart_count">2</span></Link>
+                </div>
+
+            </nav>
+        </Fragment>
+
     )
 }
 
