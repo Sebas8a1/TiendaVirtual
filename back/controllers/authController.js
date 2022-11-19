654a02046e678edc32b73111070050ba06666250
Sebas8a1 <111146914+Sebas8a1@users.noreply.github.com>
2022-11-18 21:44:56 -0500

    Update authController.js
    ok

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/auth');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
-const sentToken = require('../utils/JWTtoken');
+const sentToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
 const crypto = require('crypto');
 const cloudinary=require("cloudinary")
